0209b192ea57358c783924fea2641a9cdbff7ff3
ridwan-io <mohammedridwan.a@gmail.com>
2025-06-05 17:08:58 +0530

    code refactored and all case passing

--- lib/simple_calculator.rb
+++ lib/simple_calculator.rb
@@ -3,9 +3,13 @@ class SimpleCalculator
   def add(numbers)
     return 0 if numbers.empty?
     split_numbers = numbers.split(/[^0-9-]/)
-    negative_numbers = split_numbers.select { |number| number.to_i < 0 }
-    raise(ArgumentError, "negative numbers not allowed: #{negative_numbers.join(", ")}") unless negative_numbers.empty?
+    check_negative(split_numbers)
 
     split_numbers.map(&:to_i).sum
+  end
+
+  def check_negative(numbers)
+    negative_numbers = numbers.select { |number| number.to_i < 0 }
+    raise(ArgumentError, "negative numbers not allowed: #{negative_numbers.join(", ")}") unless negative_numbers.empty?
   end
 end
